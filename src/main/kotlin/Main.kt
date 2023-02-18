fb203c4a0a3663d547f32471a239136bd010a16d
joycewanjiku <joycemumbiiwanjiku@gmail.com>
2023-02-18 11:00:04 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(){
+    var num1 =22
+    var num2 =11
+    var  subtractio =num1 -num2
+    println ("this is the substraction of two numbers:,"+subtractio)
+
+
+    var mult1 =33
+    var mult2 =24
+    var multiply =mult1 * mult2
+    println("multiply of two numbers:,"+ multiply)
+
+    var div1 =88
+    var div2 =44
+    var division =div1 / div2
+    println("this is the division of two numbers:,"+division)
+
+    var modu1 =32
+    var modul2 =64
+    var module =modu1 % modul2
+    println("module of two numbers:,"+module)
+
+
+    var name = "akirschix"
+    println("my school name is"+name)
+
+    var friend = "kavanyana"
+    println("my name is"+friend)
+
+    var car = "bus"
+    println("car name is"+car)
+}
